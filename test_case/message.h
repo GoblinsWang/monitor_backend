9294bad12116c10b8272b2744d579644456fc535
GoblinsWang <806586736@qq.com>
2022-07-20 02:49:44 +0000

    convert to cpp

---
+++ test_case/message.h
@@ -0,0 +1,14 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+typedef struct
+{
+    int type;
+    char data[1024];
+} messageObject;
+
+#define MSG_PING 1
+#define MSG_PONG 2
+#define MSG_TYPE1 11
+#define MSG_TYPE2 21
+
+#endif // MESSAGE_H
